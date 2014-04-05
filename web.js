17f0d6941b9612c20ae49c2c7bddf31f2b776604
dkeresteci <dkeresteci@gmail.com>
2014-04-05 19:19:44 +0000

    Changed web.js to spit out index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
+var html_file = 'index.html';
+
 app.use(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var html = fs.readFileSync(html_file).toString();
+  response.send(html);
 });
 
 var port = process.env.PORT || 5000;
